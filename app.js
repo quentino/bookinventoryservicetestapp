25f7e6838c283059e5de02234c995ba05e055e1f
Michal Pawluk <micpawluk@gmail.com>
2015-12-22 22:54:01 +0100

    change mongodb database

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var app = express();
 
-
-var url = process.env.MONGO_LAB_URI || 'mongodb://heroku_4x78mgjj:5c76vmaot6lpjfdubl2k6f1qc4@ds033285.mongolab.com:33285/heroku_4x78mgjj';
+var url = process.env.MONGO_LAB_URI || 'mongodb://192.168.99.100:32770';
+console.log(url);
 var connection = MongoClient.connect(url);
 var repository = require('./stockRepository')(connection);
 //var repository = require('./inMemoryRepository');
